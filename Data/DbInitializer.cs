d46ce5a5d59f239b1eb1224d29abf1d2a20d86e2
gormrgryn <gor-margaryan-05@mail.ru>
2021-08-29 13:55:34 +0400

    basket visualization

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -16,11 +16,10 @@ namespace CoffeeShopApp.Data
             }
             var coffees = new Coffee[]
             {
-                new Coffee { Name="Iced Caffè Americano", Image="starbucks-coffee.png", Price=5, Calories=15 },
-                new Coffee { Name="Vanilla Sweet Cream Nitro Cold Brew", Image="starbucks-coffee.png", Price=10, Calories=70 },
-                new Coffee { Name="Cold Brew Coffee with Milk", Image="starbucks-coffee.png", Price=7, Calories=35 },
-                new Coffee { Name="Salted Caramel Cream Cold Brew", Image="starbucks-coffee.png", Price=12, Calories=220 },
-                new Coffee { Name="Cold Brew Coffee with Milk", Image="starbucks-coffee.png", Price=7, Calories=35 }
+                new Coffee { Name="Iced Caffè Americano", Image="iced-caffe-americano.png", Price=5, Calories=15 },
+                new Coffee { Name="Vanilla Sweet Cream Nitro Cold Brew", Image="vanilla-sweet-cream.png", Price=10, Calories=70 },
+                new Coffee { Name="Cold Brew Coffee with Milk", Image="cold-coffee-milk.png", Price=7, Calories=35 },
+                new Coffee { Name="Salted Caramel Cream Cold Brew", Image="salted-caramel-cream.png", Price=12, Calories=220 }
             };
             foreach (Product coffee in coffees)
             {
