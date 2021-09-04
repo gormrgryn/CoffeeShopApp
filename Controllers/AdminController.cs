efa1563bca2f0631b30e701766377f91cc3676a5
gormrgryn <gor-margaryan-05@mail.ru>
2021-09-04 20:01:22 +0400

    admin panel

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using CoffeeShopApp.Models;
+using CoffeeShopApp.Data;
+
+namespace CoffeeShopApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly CoffeeShopContext context;
+
+        public AdminController(CoffeeShopContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Admin Panel";
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(
+            [Bind("ProductID,Name,Image,DrinkType,Price,Calories")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(product);
+                await context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -19,7 +19,9 @@ namespace CoffeeShopApp.Data
                 new Coffee { Name="Iced Caffè Americano", Image="iced-caffe-americano.png", Price=5, Calories=15 },
                 new Coffee { Name="Vanilla Sweet Cream Nitro Cold Brew", Image="vanilla-sweet-cream.png", Price=10, Calories=70 },
                 new Coffee { Name="Cold Brew Coffee with Milk", Image="cold-coffee-milk.png", Price=7, Calories=35 },
-                new Coffee { Name="Salted Caramel Cream Cold Brew", Image="salted-caramel-cream.png", Price=12, Calories=220 }
+                new Coffee { Name="Salted Caramel Cream Cold Brew", Image="salted-caramel-cream.png", Price=12, Calories=220 },
+                new Coffee { Name="Iced Caffè Americano", Image="iced-caffe-americano.png", Price=5, Calories=15 },
+                new Coffee { Name="Vanilla Sweet Cream Nitro Cold Brew", Image="vanilla-sweet-cream.png", Price=10, Calories=70 },
             };
             foreach (Product coffee in coffees)
             {
